224cfbebb5626909918eac4a22226c63c0a3cd63
unknown <ahmostafaa94@gmail.com>
2020-11-06 21:24:40 +0200

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { EmployeesTableComponent } from './components/employees-table/employees-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    EmployeesTableComponent
   ],
   imports: [
     BrowserModule
